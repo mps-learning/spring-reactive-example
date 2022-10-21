1f9d2e01f755aedb097e49cc6b57e9982b7d71b1
Manvendra P Singh <manvendra.singh@gmail.com>
2022-10-22 01:06:42 +0530

    To safeguard Mono.zip

--- src/main/java/com/mps/springreactiveexample/service/impl/AItemSearchService.java
+++ src/main/java/com/mps/springreactiveexample/service/impl/AItemSearchService.java
@@ -5,6 +5,7 @@ import com.mps.springreactiveexample.model.*;
 import com.mps.springreactiveexample.service.ItemSearchService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 
 import java.util.List;
@@ -33,22 +34,31 @@ public class AItemSearchService implements ItemSearchService {
     //TODO: all these calls need to be asynchronous.
     @Override
     public Mono<Item> searchItem(SingleItemSearchRequest sisr) {
-        System.out.println("\t\tInside "+supportedItem()+" searchItem with thread " + Thread.currentThread().toString());
+        System.out.println("\t\tInside " + supportedItem() + " searchItem with thread " + Thread.currentThread().toString());
 
         //TODO: how to make these XXX YYY calls conditionals In clear way?
-        return Mono.zip(mockBackendApi.getItemANameApi(sisr.getItemIdentifiers().getItemId()),
-                        sisr.isAddXXXDetails()
-                        ?mockBackendApi.getItemAXXXApi(sisr.getItemIdentifiers().getItemId())
-                        :Mono.empty(),
-                        sisr.isAddYYYDetails()
-                        ?mockBackendApi.getItemAYYYApi(sisr.getItemIdentifiers().getItemId())
-                        :Mono.empty())
+        return Mono.zip(getNameDetails(sisr).defaultIfEmpty("Default Name"),
+                        getXXXDetails(sisr).defaultIfEmpty("Default XXX Details"),
+                        getYYYDetails(sisr).defaultIfEmpty("Default YYY Details"))
                 .map(tuple3 -> Item.builder()
                         .name(tuple3.getT1())
                         .xxxDetails(tuple3.getT2())
                         .yyyDetails(tuple3.getT3())
                         .build()
                 );
+    }
+    private Mono<String> getNameDetails(SingleItemSearchRequest sisr) {
+        return mockBackendApi.getItemANameApi(sisr.getItemIdentifiers().getItemId());
+    }
+    private Mono<String> getYYYDetails(SingleItemSearchRequest sisr) {
+        return sisr.isAddYYYDetails()
+               ? mockBackendApi.getItemAYYYApi(sisr.getItemIdentifiers().getItemId())
+               : Mono.empty();
+    }
+    private Mono<String> getXXXDetails(SingleItemSearchRequest sisr) {
+        return sisr.isAddXXXDetails()
+               ? mockBackendApi.getItemAXXXApi(sisr.getItemIdentifiers().getItemId())
+               : Mono.empty();
     }
 
 
--- src/main/java/com/mps/springreactiveexample/service/impl/BItemSearchService.java
+++ src/main/java/com/mps/springreactiveexample/service/impl/BItemSearchService.java
@@ -6,6 +6,7 @@ import com.mps.springreactiveexample.service.ItemSearchService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -29,22 +30,31 @@ public class BItemSearchService implements ItemSearchService {
     //TODO: all these calls need to be asynchronous.
     @Override
     public Mono<Item> searchItem(SingleItemSearchRequest sisr) {
-        System.out.println("\t\tInside "+supportedItem()+" searchItem with thread " + Thread.currentThread().toString());
+        System.out.println("\t\tInside " + supportedItem() + " searchItem with thread " + Thread.currentThread().toString());
 
         //TODO: how to make these XXX YYY calls conditionals In clear way?
-        return Mono.zip(mockBackendApi.getItemBNameApi(sisr.getItemIdentifiers().getItemId()),
-                        sisr.isAddXXXDetails()
-                        ?mockBackendApi.getItemBXXXApi(sisr.getItemIdentifiers().getItemId())
-                        :Mono.empty(),
-                        sisr.isAddYYYDetails()
-                        ?mockBackendApi.getItemBYYYApi(sisr.getItemIdentifiers().getItemId())
-                        :Mono.empty())
+        return Mono.zip(getNameDetails(sisr).defaultIfEmpty("Default Name"),
+                        getXXXDetails(sisr).defaultIfEmpty("Default XXX Details"),
+                        getYYYDetails(sisr).defaultIfEmpty("Default YYY Details"))
                 .map(tuple3 -> Item.builder()
                         .name(tuple3.getT1())
                         .xxxDetails(tuple3.getT2())
                         .yyyDetails(tuple3.getT3())
                         .build()
                 );
+    }
+    private Mono<String> getNameDetails(SingleItemSearchRequest sisr) {
+        return mockBackendApi.getItemBNameApi(sisr.getItemIdentifiers().getItemId());
+    }
+    private Mono<String> getYYYDetails(SingleItemSearchRequest sisr) {
+        return sisr.isAddYYYDetails()
+               ? mockBackendApi.getItemBYYYApi(sisr.getItemIdentifiers().getItemId())
+               : Mono.empty();
+    }
+    private Mono<String> getXXXDetails(SingleItemSearchRequest sisr) {
+        return sisr.isAddXXXDetails()
+               ? mockBackendApi.getItemBXXXApi(sisr.getItemIdentifiers().getItemId())
+               : Mono.empty();
     }
 
 
